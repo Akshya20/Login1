74c65e6539e9830935bd06693d8808f518b79241
Akshya52 <aks575994@gmail.com>
2024-12-11 11:43:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home1 from "../src/Home/Home"
+// import Page1 from "../src/Page/Page"
+
+function App() {
+  return (
+    <div>
+      <Home1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+
+
+
+
+function Home1() {
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+    const [loading, setloading] = useState(false);
+    const [message, setmessage] = useState(false);
+
+    const handleLogin = () => {
+        if (username === "user" && password === "password") {
+            localStorage.setItem("username", username);
+            setloading(true);
+        }else {
+            setmessage(true);
+        }
+    }
+
+    return (
+        <div>
+
+            {!message ? (
+                <h2>Login Page</h2>
+            ) : (
+                <>
+                 <h2>Login Page</h2>
+                 <p>Invalid Username and Password</p>
+                </>
+               
+            )}
+            {!loading ? (
+                <>
+                    <form  onSubmit={handleLogin}>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setusername(e.target.value)}
+                        required></input>
+                    <label>Password:</label>
+                    <input type="text"
+                        value={password}
+                        onChange={(e) => setpassword(e.target.value)}
+                        required>
+                        </input>
+
+                    <button type="submit">Submit</button>
+                    </form>
+
+
+                </>
+            ) : (
+                <h2>Welcome,{localStorage.getItem("username")}!</h2>
+
+            )}
+        </div>
+    );
+
+};
+
+export default Home1;
